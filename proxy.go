4db894311bd4dc71c6be13fb51bd056c9351ebbe
Pegerto Fernandez (pegferna) <pegferna@cisco.com>
2016-02-29 15:05:43 +0000

    format code as per standards

--- proxy.go
+++ proxy.go
@@ -1,41 +1,42 @@
 package main
 
-import ("io"
-        "flag"
-        "strconv"
-        "net/http"
-        "encoding/json"
-        "time"
-        "fmt"
-      )
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"time"
+)
 
 type handleConnection struct{}
 
 type Transaction struct {
-    RemoteAddr  string
-    URL  string
-    SC int32
+	RemoteAddr string
+	URL        string
+	SC         int32
 }
 
 func (*handleConnection) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  tr := http.Transport{}
-  tr.DisableKeepAlives = true
-  resp , err := tr.RoundTrip(r)
+	tr := http.Transport{}
+	tr.DisableKeepAlives = true
+	resp, err := tr.RoundTrip(r)
 
-  if err == nil {
-    copyHeaders(w.Header(), resp.Header)
+	if err == nil {
+		copyHeaders(w.Header(), resp.Header)
 		w.WriteHeader(resp.StatusCode)
 		io.Copy(w, resp.Body)
 
-    if bodyError := resp.Body.Close(); err != nil {
+		if bodyError := resp.Body.Close(); err != nil {
 			http.Error(w, bodyError.Error(), 500)
-      log(r, 500)
-		}else{
-      log(r, int32(resp.StatusCode))
-    }
-  }else {
-    http.Error(w, err.Error(), 500)
-  }
+			log(r, 500)
+		} else {
+			log(r, int32(resp.StatusCode))
+		}
+	} else {
+		http.Error(w, err.Error(), 500)
+	}
 }
 
 func copyHeaders(dst, src http.Header) {
@@ -49,22 +50,21 @@ func copyHeaders(dst, src http.Header) {
 	}
 }
 
-func log(r *http.Request, sC int32){
-  tr := Transaction{r.RemoteAddr, r.URL.String(), sC}
-  line, _ := json.Marshal(tr)
-  fmt.Println(string(line))
+func log(r *http.Request, sC int32) {
+	tr := Transaction{r.RemoteAddr, r.URL.String(), sC}
+	line, _ := json.Marshal(tr)
+	fmt.Println(string(line))
 }
 
-
 func main() {
-    httpPort := flag.Int("httpPort", 8080, "Bind port for http proxy service")
-    flag.Parse()
-    s := &http.Server{
-	     Addr:           ":"+ strconv.Itoa(*httpPort),
-       Handler:        &handleConnection{},
-	     ReadTimeout:    10 * time.Second,
-	     WriteTimeout:   10 * time.Second,
-	     MaxHeaderBytes: 1 << 20,
-     }
-     s.ListenAndServe()
+	httpPort := flag.Int("httpPort", 8080, "Bind port for http proxy service")
+	flag.Parse()
+	s := &http.Server{
+		Addr:           ":" + strconv.Itoa(*httpPort),
+		Handler:        &handleConnection{},
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	s.ListenAndServe()
 }
